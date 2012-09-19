a8595537dbabbadb9feed223f386d5ddad2d7a2a
Jack Ramey <jack.ramey@gmail.com>
2012-09-18 17:48:57 -0700

    Login now works, added changes to user.py and views.py

--- emilyandjack/user.py
+++ emilyandjack/user.py
@@ -1,26 +1,45 @@
 from flask.ext.sqlalchemy import SQLAlchemy
-from emilyandjack import app
+from emilyandjack import app, login_manager
 from database import db
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True)
-  password = db.Column(db.String(256))
-  active = db.Column(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(256))
+    displayname = db.Column(db.String(80))
+    active = db.Column(db.Boolean)
 
-  def __init__(self, username, password, active=False):
-    self.username = username
-    self.password = password
-    self.active = active
+    def __init__(self, username, password, displayname, active=False):
+        self.username = username
+        self.password = password
+        self.displayname = displayname
+        self.active = active
 
-  def __repr__(self):
-    return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r, %r, %r, %r>' % (self.username, \
+            self.password, self.displayname, self.active)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.filter_by(username=userid).first()
 
 def init_users():
-  jack = User('jack', 'omgcat', active=True)
-  emily = User('emily', 'omgcat', active=True)
-  db.session.add(jack)
-  db.session.add(emily)
-  db.session.commit()
+    jack = User('jack', 'omgcat', 'Jack', active=True)
+    emily = User('emily', 'omgcat', 'Emily', active=True)
+    db.session.add(jack)
+    db.session.add(emily)
+    db.session.commit()
 
 
