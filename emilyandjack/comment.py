7dc577930c0af7dd567c41a80bcecc381a8a6cf0
Jack Ramey <jack.ramey@gmail.com>
2013-02-01 00:23:42 -0800

    Forgot to add comment.py to repo... that caused some issues.

---
+++ emilyandjack/comment.py
@@ -0,0 +1,33 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from emilyandjack import app
+from utilities import db
+from datetime import datetime
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100))
+    body = db.Column(db.String(4096))
+    date = db.Column(db.DateTime)
+
+    def __init__(self, author, body):
+        self.author = author
+        self.body = body
+        self.date = datetime.now()
+
+    def __repr__(self):
+        return "<Comment: %r, %r>" % (self.id, self.date)
+
+    def get_id(self):
+        return self.id
+
+def get_comment(cid):
+    return Comment.query.filter_by(id=cid).first()
+
+def init_comments():
+    auth = 'Me of course!'
+    body = 'You guys are awesome'
+    c1 = Comment(auth, body)
+    db.session.add(c1)
+    db.session.commit()
+
+
